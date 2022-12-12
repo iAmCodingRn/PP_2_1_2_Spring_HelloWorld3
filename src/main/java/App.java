3f3c90092c1ae28fd9b6e4be98b605c766e70111
iAmCodingRn <111708435+iAmCodingRn@users.noreply.github.com>
2022-12-12 16:47:46 +0300

    first commit (spring_hello_world)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,22 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+
+//вызываем еще раз бин HelloWorld
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+//дальше идет сравнение 2 пар бинов по ссылке
+        System.out.println(bean == bean2);
+        System.out.println(bean);
+        System.out.println(bean2);
+// 2жды вызываем бин cat
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean.getMessage());
+        Cat bean2Cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+        System.out.println(beanCat == bean2Cat);
+
+
     }
 }
